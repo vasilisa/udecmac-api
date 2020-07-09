6464694caecbcd828d030e3e3d6f178bc354108a
vasilisa <vasilisaskv@gmail.com>
2020-07-09 10:02:45 +0200

    switching to gunicorn server

--- config_gunicorn.py
+++ config_gunicorn.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import os 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 6 
+
+# workers = multiprocessing.cpu_count() * 2 + 1
 
 # Gunicorn doesn't like our import of config, so we wrap it in a funciton.
 def from_config_file():
